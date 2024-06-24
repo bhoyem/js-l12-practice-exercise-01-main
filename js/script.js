974c5ee49f8355f78518a73a4ff79fc5ebfce647
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-06-24 15:52:31 -0500

    Completed code for exercise 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const userRequests = await fetch("https://randomuser.me/api?results=5");
+    const data = await userRequests.json();
+    // console.log(data);
+    const userResults = data.results;
+    console.log(userResults)
+    displayUsers(userResults);
+};
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    for (user of userResults) {
+        const name = user.name.first;
+        const country = user.location.country;
+        const imageUrl = user.picture.medium;
+        // console.log(name, country, imageUrl);
+        let userDiv = document.createElement("userDiv");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />`;
+        randomFolks.append(userDiv);
+    }
+};
+
+
+
+getData();
